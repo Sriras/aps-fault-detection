205b5995b4ebe7b056b3c031b1ad4e749889ea47
Sriras <shsrnmn23@gmail.com>
2023-11-25 17:37:07 +0530

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+
+
+def get_requirments():
+    pass
+
+setup(
+    
+    name='sensor',
+    version='0.0.1',
+    author='sahas',
+    author_email='shsrnmn23@gmail.com',
+    packages=find_packages(),
+    requires=get_requirments()
+    
+
+)
